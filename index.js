8adaa10a4fd7722cd3cd420c124f41d0a5d38473
MENDIRATTA NITIN <nitin.mendiratta@3ds.com>
2022-04-16 18:35:24 -0400

    coming soon added

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const challenges = [
-  { title: "Toast Popup", link: "toast-popup" },
-  { title: "Guess the number", link: "guess-the-number" },
-  { title: "Telephone formatter", link: "telephone-formatter" },
-  { title: "Light & Dark mode", link: "light-dark-mode" },
-  { title: "Password Strength", link: "password-strength" },
+	//MUST DO
+	// sadanandpai/frontend-mini-challenges
+  { title: "Toast Popup", link: "#" },
+  { title: "Toast Popup", link: "#" },
+  { title: "Guess the number", link: "#" },
+  { title: "Telephone formatter", link: "#" },
+  { title: "Light & Dark mode", link: "#" },
+  { title: "Password Strength", link: "#" },
   { title: "Loaders...", link: "#" },
-  { title: "Star Rating", link: "star-rating" },
-  { title: "Pixel Art", link: "pixel-art" },
-  { title: "Color Spotter", link: "color-spotter" },
-  { title: "Todo List", link: "todo-list" },
-  { title: "Tic-Tac-Toe", link: "tic-tac-toe" },
-  { title: "Chess board", link: "chess-board" },
+  { title: "Star Rating", link: "#" },
+  { title: "Pixel Art", link: "#" },
+  { title: "Color Spotter", link: "#" },
+  { title: "Todo List", link: "#" },
+  { title: "Tic-Tac-Toe", link: "#" },
+  { title: "Chess board", link: "#" },
   { title: "Skeleton Loader", link: "#" },
-  { title: "Countdown Timer", link: "count-down-timer" },
-  { title: "Area Selector", link: "area-selector" },
-  { title: "Carousel", link: "carousel" },
-  { title: "Paginator", link: "paginator" },
-  { title: "Comment box", link: "comment-box" },
-  { title: "Array methods", link: "array-methods" },
+  { title: "Countdown Timer", link: "#" },
+  { title: "Area Selector", link: "#" },
+  { title: "Carousel", link: "#" },
+  { title: "Paginator", link: "#" },
+  { title: "Comment box", link: "#" },
+  { title: "Array methods", link: "#" },
   { title: "Stepper", link: "#" },
   { title: "Accordion", link: "#" },
   { title: "Transfer List", link: "#" },
@@ -27,23 +30,39 @@ const challenges = [
   { title: "Sort Table", link: "#" },
   { title: "Calendar", link: "#" },
   { title: "Emoji Editor", link: "#" },
+  
+  //Brad Traversy: 20+ Web Projects With Vanilla JavaScript
+
+  // ZTM: JavaScript Web Projects: 20 Projects to Build Your Portfolio
+
+  // Brad Traversy: 50 Projects In 50 Days - HTML, CSS & JavaScript
+  { title: "Expanding Cards", link: "expanding-cards"},
+  { title: "Progress Steps", link: "progress-steps"},
+  // JavaScript30
+
 ];
+
+const challengeGridEl = document.getElementById("challengeGrid");
+const comingsoonGridEl = document.getElementById("comingsoonGrid");
 
 const createAnchorElement = (obj) => {
   const a = document.createElement("a");
-  a.textContent = obj.title;
-  a.href = `./mc/${obj.link}/`;
+  
 
   if(obj.link === '#'){
+    a.textContent = obj.title;
     a.classList.add('disabled');
     a.title = 'To be developed';
     a.href = '#';
+	comingsoonGridEl.appendChild(a);
+  }else{
+	  a.textContent = obj.title;
+	  a.href = `./challenges/${obj.link}/`;
+	  challengeGridEl.appendChild(a);
   }
-
   return a;
 };
 
-const challengeGridEl = document.getElementById("challengeGrid");
-challenges
-  .map(createAnchorElement)
-  .forEach((el) => challengeGridEl.appendChild(el));
+
+
+challenges.forEach((obj) => createAnchorElement(obj));
